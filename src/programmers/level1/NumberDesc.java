3cc0ea1d54b0e3d62b9de11a96f20786767bae92
nomad411 <nomad411@naver.com>
2022-06-03 13:44:48 +0900

    Update NumberDesc.java
    정수 내림차순 정렬 완료

--- src/programmers/level1/NumberDesc.java
+++ src/programmers/level1/NumberDesc.java
@@ -4,8 +4,6 @@
 package programmers.level1;
 
 import java.util.Arrays;
-import java.util.Collections;
-
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/12933
  * 정수 내림차순으로 배치하기 118372
@@ -20,22 +18,32 @@ public class NumberDesc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		desc(118372);
+		System.out.println(desc(118372));
 	}
 	
-	public static int desc(int num) {
-		int result = 0;
-		String temp = Integer.toString(num);
-		Integer[] tempArr = new Integer[temp.length()];
+	public static long desc(long num) {
+		// long -> String
+		String temp = String.valueOf(num);
 		
+		// char 배열 생성
+		char[] tempArr = new char[temp.length()];
+		
+		// 배열에 할당
 		for (int i = 0; i < temp.length(); i++) {
-			tempArr[i] = temp.charAt(i) - '0';
+			tempArr[i] = temp.charAt(i);
 		}
-		Arrays.sort(tempArr, Collections.reverseOrder());
 		
+		// 정렬
+		Arrays.sort(tempArr);
 		
+		StringBuilder sb = new StringBuilder();
 		
-		return result;
+		// 내림차순 정렬 및 SB 변환
+		for (int i = tempArr.length -1; i >= 0; i--) {
+			sb.append(tempArr[i]);
+		}
+	
+		return Long.parseLong(sb.toString());
 	}
 
 }
