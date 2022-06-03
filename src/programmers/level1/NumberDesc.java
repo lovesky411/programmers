af0f6b040a0c37d15b3f58917228b796c0a99340
nomad411 <nomad411@naver.com>
2022-06-03 09:17:13 +0900

    정수 내림차순 배치
    수정 중

--- src/programmers/level1/NumberDesc.java
+++ src/programmers/level1/NumberDesc.java
@@ -3,9 +3,12 @@
  */
 package programmers.level1;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/12933
- * 정수 내림차순으로 배치하기
+ * 정수 내림차순으로 배치하기 118372
  * @author Seung-Ho Ka
  *
  */
@@ -17,6 +20,22 @@ public class NumberDesc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		desc(118372);
+	}
+	
+	public static int desc(int num) {
+		int result = 0;
+		String temp = Integer.toString(num);
+		Integer[] tempArr = new Integer[temp.length()];
+		
+		for (int i = 0; i < temp.length(); i++) {
+			tempArr[i] = temp.charAt(i) - '0';
+		}
+		Arrays.sort(tempArr, Collections.reverseOrder());
+		
+		
+		
+		return result;
 	}
 
 }
