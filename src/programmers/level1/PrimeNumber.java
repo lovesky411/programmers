ff28fc880b189e0fd93dafc7b3772fe91c6666d0
nomad411 <nomad411@naver.com>
2022-06-14 09:04:12 +0900

    문자열을 정수로 바꾸기

---
+++ src/programmers/level1/PrimeNumber.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package programmers.level1;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12921
+ * 소수 찾기
+ * @author Seung-Ho Ka
+ *
+ */
+public class PrimeNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		solution(10);
+	}
+	
+	public static int solution(int n) {
+        int answer = 0;
+        
+        return answer;
+    }
+
+}
