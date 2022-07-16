9dda5cd671289cf3dd04b552e125010722f1f096
ksh <nomad411@naver.com>
2022-07-16 12:00:45 +0900

    문자열 내 p와 y의 개수

--- src/programmers/level1/PYInWord.java
+++ src/programmers/level1/PYInWord.java
@@ -3,7 +3,7 @@ package programmers.level1;
 /**
  * 문자열 내 p와 y의 개수
  * https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=java
- * @author ksh
+ * @author ksh 
  *
  */
 public class PYInWord {
