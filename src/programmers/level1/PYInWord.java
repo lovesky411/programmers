5a4c349f7f852aa6ade29fac7ca90c3aad707d05
ksh <nomad411@naver.com>
2022-07-16 11:54:58 +0900

    LEVEL 1 ) 문자열 내 p와 y의 개수

---
+++ src/programmers/level1/PYInWord.java
@@ -0,0 +1,36 @@
+package programmers.level1;
+
+public class PYInWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution("pPoooyY"));
+		System.out.println(solution("Pyy"));
+
+	}
+	
+	static boolean solution(String s) {
+	    
+	    int pCnt = 0;
+	    int yCnt = 0;
+	    
+	    char[] cArr = (s.toLowerCase()).toCharArray();
+	    
+	    for (char c : cArr) {
+	    	if(c == 'p') {
+	    		pCnt++;
+	    	}
+	    	if(c == 'y') {
+	    		yCnt++;
+	    	}
+		}
+	    
+	    if(pCnt == yCnt) {
+	    	return true;
+	    }else {
+	    	return false;
+	    }
+	    
+	}
+
+}
